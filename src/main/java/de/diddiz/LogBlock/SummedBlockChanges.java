ce77199e05f2b72435a8b2716bbfad3071594269
TomyLobo <tomylobo@nurfuerspam.de>
2014-01-15 20:53:49 +0100

    Made "sum blocks/players" entries clickable.

--- src/main/java/de/diddiz/LogBlock/SummedBlockChanges.java
+++ src/main/java/de/diddiz/LogBlock/SummedBlockChanges.java
@@ -5,7 +5,6 @@ import static de.diddiz.util.Utils.spaces;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import de.diddiz.LogBlock.html.Parser;
 import org.bukkit.Location;
 import de.diddiz.LogBlock.QueryParams.SummarizationMode;
 
@@ -14,9 +13,11 @@ public class SummedBlockChanges extends AbstractLookupCacheElement
 	private final String group;
 	private final int created, destroyed;
 	private final float spaceFactor;
+	private final boolean isPlayers;
 
 	public SummedBlockChanges(ResultSet rs, QueryParams p, float spaceFactor) throws SQLException {
-		group = p.sum == SummarizationMode.PLAYERS ? rs.getString(1) : materialName(rs.getInt(1));
+		isPlayers = p.sum == SummarizationMode.PLAYERS;
+		group = isPlayers ? rs.getString(1) : materialName(rs.getInt(1));
 		created = rs.getInt(2);
 		destroyed = rs.getInt(3);
 		this.spaceFactor = spaceFactor;
@@ -34,10 +35,14 @@ public class SummedBlockChanges extends AbstractLookupCacheElement
 
 	@Override
 	public String getXmlMessage() {
+		String message = created + spaces((int) ((10 - String.valueOf(created).length()) / spaceFactor)) + destroyed + spaces((int) ((10 - String.valueOf(destroyed).length()) / spaceFactor));
+
+		final String command = String.format("/lb last %s %s sum none coords", isPlayers ? "player" : "block", group.replace(' ', '_'));
+		message = String.format("%1$s<span onClick=\"run_command('%2$s')\" onHover=\"show_text('Click to run\n&lt;color name=&quot;blue&quot;>%2$s&lt;/color>')\">%3$s</span>", message, command, group);
 		if (created < destroyed) {
-			return String.format("<color name=\"red\">%1$s</color>", Parser.escape(getMessage()));
+			message = String.format("<color name=\"red\">%1$s</color>", message);
 		}
 
-		return super.getXmlMessage();
+		return message;
 	}
 }
